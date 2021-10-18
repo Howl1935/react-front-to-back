749afc97958c8a995008cc8a23155f9fe8e11312
Howl1935 <travisphawley@gmail.com>
2021-10-17 21:26:05 -0700

    All functioning, now incorporating hooks

--- src/App.js
+++ src/App.js
@@ -14,8 +14,10 @@ class App extends Component{
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
+   
   }
 
   //search Github users
@@ -33,6 +35,15 @@ class App extends Component{
     .get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
     this.setState({ user: res.data, loading: false })    
   };
+
+//get user repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios
+    .get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({ repos: res.data, loading: false })    
+  };
+
   //clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
   //set alert
@@ -42,7 +53,7 @@ class App extends Component{
   } 
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
     return (
       <Router>
         <div className="App">
@@ -63,7 +74,7 @@ class App extends Component{
                 )} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' render={ props => (
-                  <User { ...props } getUser={this.getUser} user={user} loading={loading}/>
+                  <User { ...props } getUser={this.getUser} getUserRepos={this.getUserRepos} user={user} repos={repos} loading={loading}/>
                 )} />
                 </Switch>
 
