09a73eae824e882e08aacd98eec1a37c1a92e3d2
Howl1935 <travisphawley@gmail.com>
2021-10-23 15:01:21 -0700

    updated again to use more hooks and working on context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,22 +1,15 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Spinner from '../layout/Spinner';
 import  Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-export class User extends Component {
-    componentDidMount(){
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user:PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired
-    }
-    render() {
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+        // esline-disable-next-line
+    }, []);
         const {
             name, 
             avatar_url,
@@ -31,9 +24,8 @@ export class User extends Component {
             public_repos, 
             public_gists, 
             hireable
-        } = this.props.user;
+        } = user;
 
-        const { loading, repos } = this.props;
         if(loading) return <Spinner/>;
         return (
             <Fragment>
@@ -79,7 +71,15 @@ export class User extends Component {
                 <Repos repos={repos} />
             </Fragment>
         );
-    };
+
+}
+
+User.propTypes = {
+    loading: PropTypes.bool,
+    user:PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
 }
 
 export default User
